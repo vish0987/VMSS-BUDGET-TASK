cde408a1b4beaed35b5f2ea4ad454b285ec061a0
muzzammil-hamdu <shaikhussain752@gmail.com>
2025-08-20 17:44:16 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,64 +1,55 @@
 resource "azurerm_resource_group" "rg" {
-name = "rg-vmss-demo"
-location = "uk south"
+  name     = "rg-vmss-demo"
+  location = "uk south"
 }
-
 
 resource "azurerm_virtual_network" "vnet" {
-name = "vnet-demo"
-address_space = ["10.0.0.0/16"]
-location = azurerm_resource_group.rg.location
-resource_group_name = azurerm_resource_group.rg.name
+  name                = "vnet-demo"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
-
 
 resource "azurerm_subnet" "subnet" {
-name = "subnet-demo"
-resource_group_name = azurerm_resource_group.rg.name
-virtual_network_name = azurerm_virtual_network.vnet.name
-address_prefixes = ["10.0.1.0/24"]
+  name                 = "subnet-demo"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
-
 
 resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
-name = "vmss-demo"
-location = azurerm_resource_group.rg.location
-resource_group_name = azurerm_resource_group.rg.name
-sku = "Standard_B1s"
-instances = 1
-admin_username = "azureuser"
+  name                = "vmss-demo"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard_B1s"
+  instances           = 1
+  admin_username      = "hussain"  
 
+  admin_ssh_key {
+    username   = "hussain"   
+    public_key = file("/home/mujju/.ssh/id_rsa.pub")
+  }
 
-admin_ssh_key {
-username = "azureuser"
-public_key = "ssh-rsa REPLACE_ME"
-}
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
 
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
 
-source_image_reference {
-publisher = "Canonical"
-offer = "0001-com-ubuntu-server-focal"
-sku = "20_04-lts"
-version = "latest"
-}
+  network_interface {
+    name    = "nic"
+    primary = true
+    ip_configuration {
+      name      = "internal"
+      primary   = true
+      subnet_id = azurerm_subnet.subnet.id
+    }
+  }
 
-
-os_disk {
-storage_account_type = "Standard_LRS"
-caching = "ReadWrite"
-}
-
-
-network_interface {
-name = "nic"
-primary = true
-ip_configuration {
-name = "internal"
-primary = true
-subnet_id = azurerm_subnet.subnet.id
-}
-}
-
-
-tags = { costScope = "vmss-demo" }
-}
+  
