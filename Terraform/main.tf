8329baf15079fc47310d807dc72f8a94c749c062
muzzammil-hamdu <shaikhussain752@gmail.com>
2025-08-20 08:50:52 +0530

    Create main.tf

---
+++ Terraform/main.tf
@@ -0,0 +1,64 @@
+resource "azurerm_resource_group" "rg" {
+name = "rg-vmss-demo"
+location = "eastus"
+}
+
+
+resource "azurerm_virtual_network" "vnet" {
+name = "vnet-demo"
+address_space = ["10.0.0.0/16"]
+location = azurerm_resource_group.rg.location
+resource_group_name = azurerm_resource_group.rg.name
+}
+
+
+resource "azurerm_subnet" "subnet" {
+name = "subnet-demo"
+resource_group_name = azurerm_resource_group.rg.name
+virtual_network_name = azurerm_virtual_network.vnet.name
+address_prefixes = ["10.0.1.0/24"]
+}
+
+
+resource "azurerm_linux_virtual_machine_scale_set" "vmss" {
+name = "vmss-demo"
+location = azurerm_resource_group.rg.location
+resource_group_name = azurerm_resource_group.rg.name
+sku = "Standard_B1s"
+instances = 1
+admin_username = "azureuser"
+
+
+admin_ssh_key {
+username = "azureuser"
+public_key = "ssh-rsa REPLACE_ME"
+}
+
+
+source_image_reference {
+publisher = "Canonical"
+offer = "0001-com-ubuntu-server-focal"
+sku = "20_04-lts"
+version = "latest"
+}
+
+
+os_disk {
+storage_account_type = "Standard_LRS"
+caching = "ReadWrite"
+}
+
+
+network_interface {
+name = "nic"
+primary = true
+ip_configuration {
+name = "internal"
+primary = true
+subnet_id = azurerm_subnet.subnet.id
+}
+}
+
+
+tags = { costScope = "vmss-demo" }
+}
